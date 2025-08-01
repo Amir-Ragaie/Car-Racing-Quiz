e9347c39103e9ec65ea6a04759858e5742c21d21
Amir-Ragaie <amir.ragaie.soliman@gmail.com>
2025-08-02 01:06:56 +0300

    done, ready for questions

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -9,62 +9,88 @@
 
       </div>
       <div class="categories" v-if="alakhdar">
-        <div class="text" v-bind:class="{ fade: categories['الاخضر'][1] }" @click="toggleCategory('الاخضر', 1)">معلومات عن القيامة</div>
-        <div class="text" v-bind:class="{ fade: categories['الاخضر'][2] }" @click="toggleCategory('الاخضر', 2)">انا مين؟ 1</div>
-        <div class="text" v-bind:class="{ fade: categories['الاخضر'][3] }" @click="toggleCategory('الاخضر', 3)">انا مين؟ 2</div>
-        <div class="text" v-bind:class="{ fade: categories['الاخضر'][4] }" @click="toggleCategory('الاخضر', 4)">انا مين؟ 3</div>
-        <div class="text" v-bind:class="{ fade: categories['الاخضر'][5] }" @click="toggleCategory('الاخضر', 5)">طقس</div>
-        <div class="text" v-bind:class="{ fade: categories['الاخضر'][6] }" @click="toggleCategory('الاخضر', 6)">معلومات عامة</div>
+        <div class="text" v-bind:class="{ fade: categories['الاخضر'][1] }" @click="toggleCategory('الاخضر', 1)">طقس</div>
+        <div class="text" v-bind:class="{ fade: categories['الاخضر'][2] }" @click="toggleCategory('الاخضر', 2)">الحان </div>
+        <div class="text" v-bind:class="{ fade: categories['الاخضر'][3] }" @click="toggleCategory('الاخضر', 3)">فوازير</div>
+        <div class="text" v-bind:class="{ fade: categories['الاخضر'][4] }" @click="toggleCategory('الاخضر', 4)">طلبات </div>
+        <div class="text" v-bind:class="{ fade: categories['الاخضر'][5] }" @click="toggleCategory('الاخضر', 5)">معلومات رياضية 2</div>
+        <div class="text" v-bind:class="{ fade: categories['الاخضر'][6] }" @click="toggleCategory('الاخضر', 6)">معلومات رياضية 1</div>
+        <div class="text" v-bind:class="{ fade: categories['الاخضر'][7] }" @click="toggleCategory('الاخضر', 7)">معلومات عامة 2</div>
+        <div class="text" v-bind:class="{ fade: categories['الاخضر'][8] }" @click="toggleCategory('الاخضر', 8)">معلومات عامة 1</div>
+        <div class="text" v-bind:class="{ fade: categories['الاخضر'][9] }" @click="toggleCategory('الاخضر', 9)">2 معلومات دينية</div>
+        <div class="text" v-bind:class="{ fade: categories['الاخضر'][10] }" @click="toggleCategory('الاخضر', 10)"> معلومات دينية 1</div>
+
       </div>
       <div class="categories" v-else-if="alahmar">
-        <div class="text" v-bind:class="{ fade: categories['الاحمر'][1] }" @click="toggleCategory('الاحمر', 1)"> القيامة 1</div>
-        <div class="text" v-bind:class="{ fade: categories['الاحمر'][2] }" @click="toggleCategory('الاحمر', 2)"> القيامة 2</div>
-        <div class="text" v-bind:class="{ fade: categories['الاحمر'][3] }" @click="toggleCategory('الاحمر', 3)">صح و غلط</div>
-        <div class="text" v-bind:class="{ fade: categories['الاحمر'][4] }" @click="toggleCategory('الاحمر', 4)">شخصيات كتابية 1</div>
-        <div class="text" v-bind:class="{ fade: categories['الاحمر'][5] }" @click="toggleCategory('الاحمر', 5)">شخصيات كتابية 2</div>
-        <div class="text" v-bind:class="{ fade: categories['الاحمر'][6] }" @click="toggleCategory('الاحمر', 6)">شخصيات كتابية 3</div>
+        <div class="text" v-bind:class="{ fade: categories['الاحمر'][1] }" @click="toggleCategory('الاحمر', 1)">طقس</div>
+        <div class="text" v-bind:class="{ fade: categories['الاحمر'][2] }" @click="toggleCategory('الاحمر', 2)">الحان </div>
+        <div class="text" v-bind:class="{ fade: categories['الاحمر'][3] }" @click="toggleCategory('الاحمر', 3)">فوازير</div>
+        <div class="text" v-bind:class="{ fade: categories['الاحمر'][4] }" @click="toggleCategory('الاحمر', 4)">طلبات </div>
+        <div class="text" v-bind:class="{ fade: categories['الاحمر'][5] }" @click="toggleCategory('الاحمر', 5)">معلومات رياضية 2</div>
+        <div class="text" v-bind:class="{ fade: categories['الاحمر'][6] }" @click="toggleCategory('الاحمر', 6)">معلومات رياضية 1</div>
+        <div class="text" v-bind:class="{ fade: categories['الاحمر'][7] }" @click="toggleCategory('الاحمر', 7)">معلومات عامة 2</div>
+        <div class="text" v-bind:class="{ fade: categories['الاحمر'][8] }" @click="toggleCategory('الاحمر', 8)">معلومات عامة 1</div>
+        <div class="text" v-bind:class="{ fade: categories['الاحمر'][9] }" @click="toggleCategory('الاحمر', 9)">2 معلومات دينية</div>
+        <div class="text" v-bind:class="{ fade: categories['الاحمر'][10] }" @click="toggleCategory('الاحمر', 10)"> معلومات دينية 1</div>
+
       </div>
       <div class="categories" v-if="alazraa">
-        <div class="text" v-bind:class="{ fade: categories['الازرق'][1] }" @click="toggleCategory('الازرق', 1)"> القيامة 1</div>
-        <div class="text" v-bind:class="{ fade: categories['الازرق'][2] }" @click="toggleCategory('الازرق', 2)"> القيامة 2</div>
-        <div class="text" v-bind:class="{ fade: categories['الازرق'][3] }" @click="toggleCategory('الازرق', 3)"> القيامة 3</div>
-        <div class="text" v-bind:class="{ fade: categories['الازرق'][4] }" @click="toggleCategory('الازرق', 4)">طقس</div>
-        <div class="text" v-bind:class="{ fade: categories['الازرق'][5] }" @click="toggleCategory('الازرق', 5)">الكتاب المقدس</div>
-        <div class="text" v-bind:class="{ fade: categories['الازرق'][6] }" @click="toggleCategory('الازرق', 6)">معلومات عامة</div>
-      </div>
+        <div class="text" v-bind:class="{ fade: categories['الازرق'][1] }" @click="toggleCategory('الازرق', 1)">طقس</div>
+        <div class="text" v-bind:class="{ fade: categories['الازرق'][2] }" @click="toggleCategory('الازرق', 2)">الحان </div>
+        <div class="text" v-bind:class="{ fade: categories['الازرق'][3] }" @click="toggleCategory('الازرق', 3)">فوازير</div>
+        <div class="text" v-bind:class="{ fade: categories['الازرق'][4] }" @click="toggleCategory('الازرق', 4)">طلبات </div>
+        <div class="text" v-bind:class="{ fade: categories['الازرق'][5] }" @click="toggleCategory('الازرق', 5)">معلومات رياضية 2</div>
+        <div class="text" v-bind:class="{ fade: categories['الازرق'][6] }" @click="toggleCategory('الازرق', 6)">معلومات رياضية 1</div>
+        <div class="text" v-bind:class="{ fade: categories['الازرق'][7] }" @click="toggleCategory('الازرق', 7)">معلومات عامة 2</div>
+        <div class="text" v-bind:class="{ fade: categories['الازرق'][8] }" @click="toggleCategory('الازرق', 8)">معلومات عامة 1</div>
+        <div class="text" v-bind:class="{ fade: categories['الازرق'][9] }" @click="toggleCategory('الازرق', 9)">2 معلومات دينية</div>
+        <div class="text" v-bind:class="{ fade: categories['الازرق'][10] }" @click="toggleCategory('الازرق', 10)"> معلومات دينية 1</div>
+</div>
       <div class="categories" v-if="alasfar">
-        <div class="text" v-bind:class="{ fade: categories['الاصفر'][1] }" @click="toggleCategory('الاصفر', 1)"> القيامة 1</div>
-        <div class="text" v-bind:class="{ fade: categories['الاصفر'][2] }" @click="toggleCategory('الاصفر', 2)"> القيامة 2</div>
-        <div class="text" v-bind:class="{ fade: categories['الاصفر'][3] }" @click="toggleCategory('الاصفر', 3)"> القيامة 3</div>
-        <div class="text" v-bind:class="{ fade: categories['الاصفر'][4] }" @click="toggleCategory('الاصفر', 4)">طقس</div>
-        <div class="text" v-bind:class="{ fade: categories['الاصفر'][5] }" @click="toggleCategory('الاصفر', 5)">شخصيات كتابية</div>
-        <div class="text" v-bind:class="{ fade: categories['الاصفر'][6] }" @click="toggleCategory('الاصفر', 6)">صح و غلط</div>
-      </div>
+        <div class="text" v-bind:class="{ fade: categories['الاصفر'][1] }" @click="toggleCategory('الاصفر', 1)">طقس</div>
+        <div class="text" v-bind:class="{ fade: categories['الاصفر'][2] }" @click="toggleCategory('الاصفر', 2)">الحان </div>
+        <div class="text" v-bind:class="{ fade: categories['الاصفر'][3] }" @click="toggleCategory('الاصفر', 3)">فوازير</div>
+        <div class="text" v-bind:class="{ fade: categories['الاصفر'][4] }" @click="toggleCategory('الاصفر', 4)">طلبات </div>
+        <div class="text" v-bind:class="{ fade: categories['الاصفر'][5] }" @click="toggleCategory('الاصفر', 5)">معلومات رياضية 2</div>
+        <div class="text" v-bind:class="{ fade: categories['الاصفر'][6] }" @click="toggleCategory('الاصفر', 6)">معلومات رياضية 1</div>
+        <div class="text" v-bind:class="{ fade: categories['الاصفر'][7] }" @click="toggleCategory('الاصفر', 7)">معلومات عامة 2</div>
+        <div class="text" v-bind:class="{ fade: categories['الاصفر'][8] }" @click="toggleCategory('الاصفر', 8)">معلومات عامة 1</div>
+        <div class="text" v-bind:class="{ fade: categories['الاصفر'][9] }" @click="toggleCategory('الاصفر', 9)">2 معلومات دينية</div>
+        <div class="text" v-bind:class="{ fade: categories['الاصفر'][10] }" @click="toggleCategory('الاصفر', 10)"> معلومات دينية 1</div>
+</div>
     </div>
-     <div class="scores-partition">
+         <div class="scores-partition">
       <div class="outer-space"></div>
       <div class="car-container">
         <div style="color: white; font-weight: bold"> الاخضر </div>
         <br>
-        <div @click="open1" class="jumping-object1" :style="{ width: objectWidth + 'px', height: objectWidth + 'px', bottom: correctAnswers1 === totalScores ? maxHeight + 'px' : step * correctAnswers1 + 'px' }"></div>
+        <div @click="open1" class="jumping-object1" :style="{ width: objectWidth + 'px', height: objectWidth + 'px', bottom: correctAnswers1 === totalScores ? maxHeight + 'px' : step * correctAnswers1 + 'px' }">
+          <div class="score-display">{{ correctAnswers1 }}</div>
+        </div>
       </div>
       <div class="double-space"></div>
       <div class="car-container">
         <div style="color: white; font-weight: bold">الاحمر </div>
         <br>
-        <div @click="open2" class="jumping-object2" :style="{ width: objectWidth + 'px', height: objectWidth + 'px', bottom: correctAnswers2 === totalScores ? maxHeight + 'px' : step * correctAnswers2 + 'px' }"></div>
+        <div @click="open2" class="jumping-object2" :style="{ width: objectWidth + 'px', height: objectWidth + 'px', bottom: correctAnswers2 === totalScores ? maxHeight + 'px' : step * correctAnswers2 + 'px' }">
+          <div class="score-display">{{ correctAnswers2 }}</div>
+        </div>
       </div>
       <div class="double-space"></div>
       <div class="car-container">
         <div style="color: white; font-weight: bold"> الازرق </div>
         <br>
-        <div @click="open3" class="jumping-object3" :style="{ width: objectWidth + 'px', height: objectWidth + 'px', bottom: correctAnswers3 === totalScores ? maxHeight + 'px' : step * correctAnswers3 + 'px' }"></div>
+        <div @click="open3" class="jumping-object3" :style="{ width: objectWidth + 'px', height: objectWidth + 'px', bottom: correctAnswers3 === totalScores ? maxHeight + 'px' : step * correctAnswers3 + 'px' }">
+          <div class="score-display">{{ correctAnswers3 }}</div>
+        </div>
       </div>
       <div class="double-space"></div>
       <div class="car-container">
         <div style="color: white; font-weight: bold"> الاصفر</div>
         <br>
-        <div @click="open4" class="jumping-object4" :style="{ width: objectWidth + 'px', height: objectWidth + 'px', bottom: correctAnswers4 === totalScores ? maxHeight + 'px' : step * correctAnswers4 + 'px' }"></div>
+        <div @click="open4" class="jumping-object4" :style="{ width: objectWidth + 'px', height: objectWidth + 'px', bottom: correctAnswers4 === totalScores ? maxHeight + 'px' : step * correctAnswers4 + 'px' }">
+          <div class="score-display">{{ correctAnswers4 }}</div>
+        </div>
       </div>
       <div class="outer-space"></div>
     </div>
@@ -85,10 +111,10 @@ export default {
       alahmar: false,
       alasfar: false,
       categories: {
-        'الاخضر': {1: false, 2: false, 3: false, 4: false, 5: false, 6: false},
-        'الاحمر': {1: false, 2: false, 3: false, 4: false, 5: false, 6: false},
-        'الازرق': {1: false, 2: false, 3: false, 4: false, 5: false, 6: false},
-        'الاصفر': {1: false, 2: false, 3: false, 4: false, 5: false, 6: false},
+        'الاخضر': {1: false, 2: false, 3: false, 4: false, 5: false, 6: false, 7: false, 8: false, 9: false, 10: false },
+        'الاحمر': {1: false, 2: false, 3: false, 4: false, 5: false, 6: false, 7: false, 8: false, 9: false, 10: false },
+        'الازرق': {1: false, 2: false, 3: false, 4: false, 5: false, 6: false, 7: false, 8: false, 9: false, 10: false },
+        'الاصفر': {1: false, 2: false, 3: false, 4: false, 5: false, 6: false, 7: false, 8: false, 9: false, 10: false },
       },
       correctAnswers1: 0,
       correctAnswers2: 0,
@@ -121,6 +147,22 @@ export default {
           6: {
             question: 'مين التلميذين اللي جريوا على القبر ولقوا القبر فاضي؟',
             answer: 'بطرس و يوحنا'
+          },
+          7: {
+            question: 'قابلت المسيح وكنت فاكراه البستاني؟',
+            answer: 'مريم المجدلية'
+          },
+          8: {
+            question: 'مكنتش مصدق ان المسيح قام والسيد المسيح خلاني اشوف أماكن المسامير؟',
+            answer: 'توما'
+          },
+          9: {
+            question: `<span>بنصلي قداس القيامة بالطقس</span><br><span>(الفرايحي -الحزايني - السنوي)</span>`,
+            answer: 'الفرايحي'
+          },
+          10: {
+            question: 'مين التلميذين اللي جريوا على القبر ولقوا القبر فاضي؟',
+            answer: 'بطرس و يوحنا'
           }
         },
         'الاحمر': {
@@ -147,6 +189,22 @@ export default {
           6: {
             question: 'مين كان موجود تحت الصليب ؟',
             answer: 'ماما العدرا و يوحنا الحبيب'
+          },
+          7: {
+            question: 'قابلت المسيح وكنت فاكراه البستاني؟',
+            answer: 'مريم المجدلية'
+          },
+          8: {
+            question: 'مكنتش مصدق ان المسيح قام والسيد المسيح خلاني اشوف أماكن المسامير؟',
+            answer: 'توما'
+          },
+          9: {
+            question: `<span>بنصلي قداس القيامة بالطقس</span><br><span>(الفرايحي -الحزايني - السنوي)</span>`,
+            answer: 'الفرايحي'
+          },
+          10: {
+            question: 'مين التلميذين اللي جريوا على القبر ولقوا القبر فاضي؟',
+            answer: 'بطرس و يوحنا'
           }
         },
          'الازرق': {
@@ -173,6 +231,22 @@ export default {
           6: {
             question: `<span>ايه الرقم اللي مش مكتوب على ولا ورقة فلوس في مصر</span><br><span>(5 - 20 - 30 - 50)</span>`,
             answer: '30'
+          },
+          7: {
+            question: 'قابلت المسيح وكنت فاكراه البستاني؟',
+            answer: 'مريم المجدلية'
+          },
+          8: {
+            question: 'مكنتش مصدق ان المسيح قام والسيد المسيح خلاني اشوف أماكن المسامير؟',
+            answer: 'توما'
+          },
+          9: {
+            question: `<span>بنصلي قداس القيامة بالطقس</span><br><span>(الفرايحي -الحزايني - السنوي)</span>`,
+            answer: 'الفرايحي'
+          },
+          10: {
+            question: 'مين التلميذين اللي جريوا على القبر ولقوا القبر فاضي؟',
+            answer: 'بطرس و يوحنا'
           }
         },
          'الاصفر': {
@@ -199,6 +273,22 @@ export default {
           6: {
             question: 'من أي ماده يتم تصنيع الزجاج',
             answer: 'الرمال'
+          },
+          7: {
+            question: 'قابلت المسيح وكنت فاكراه البستاني؟',
+            answer: 'مريم المجدلية'
+          },
+          8: {
+            question: 'مكنتش مصدق ان المسيح قام والسيد المسيح خلاني اشوف أماكن المسامير؟',
+            answer: 'توما'
+          },
+          9: {
+            question: `<span>بنصلي قداس القيامة بالطقس</span><br><span>(الفرايحي -الحزايني - السنوي)</span>`,
+            answer: 'الفرايحي'
+          },
+          10: {
+            question: 'مين التلميذين اللي جريوا على القبر ولقوا القبر فاضي؟',
+            answer: 'بطرس و يوحنا'
           }
         }
       }
@@ -578,6 +668,26 @@ div img {
 .categories :hover{
   cursor: pointer;
 }
+
+.score-display {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: white;
+  color: black;
+  font-weight: bold;
+  font-size: 24px;
+  padding: 5px 10px;
+  border-radius: 10px;
+  min-width: 40px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 3px solid black;
+}
+
 .scores-partition {
     flex: 0 0 35%;
   display: flex;
